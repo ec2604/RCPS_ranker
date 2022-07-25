Fix UT problem (features were given isntead of label). Fix numerical stability for hbm. Add brentq for finding r+ Switch to logistic ranking

concentration_inequalities.py
@@ -8,12 +8,12 @@ def h_1(a, b):
 
 
 def naive_hoeffding(n, r_lambda, alpha):
-    return -n * h_1(np.max([r_lambda.astype(np.float), alpha]), alpha)
+    return -n * h_1(np.min([r_lambda.astype(np.float), alpha]), alpha)
 
 
 def bentkus(n, alpha, r_lambda):
     rv = binom(n, alpha)
-    return 1 + np.log(rv.cdf(np.ceil(n * r_lambda)))
+    return 1 + np.log(np.max([rv.cdf(np.ceil(n * r_lambda)),1e-10]))
 
 
 def g(nu):
@@ -21,12 +21,12 @@ def g(nu):
 
 
 def maurer(nu, alpha, r_lambda, n):
-    return (-n * nu / 2) * (alpha / (1 + 2 * g(nu)) - np.max([r_lambda, alpha]))
+    return (-n * nu / 2) * (alpha / (1 + 2 * g(nu)) - np.min([r_lambda, alpha]))
 
 
 def hbm(n, r_lambda, alpha, delta):
     m = np.floor(n / 2)
-    nh_res = naive_hoeffding(m, r_lambda, alpha)
+    nh_res = naive_hoeffding(m, np.max([r_lambda,1e-10]), alpha)
     bentkus_res = bentkus(m, alpha, r_lambda)
     curr_maurer = lambda nu: maurer(nu, alpha, r_lambda, n)
     maurer_res = fminbound(curr_maurer, 0, 1, full_output=True)[1]
@@ -40,16 +40,10 @@ def rcps_lambda(n, alpha, lmbd, risk_evaluator, delta):
 
 def find_lambda_ucb(n, lmbd, risk_evaluator, delta):
     optimized_func = lambda alpha: rcps_lambda(n, alpha, lmbd, risk_evaluator, delta)
-    return brentq(optimized_func, 1e-10, 1 - 1e-10)
+    res = brentq(optimized_func, 1e-10, 1-1e-12)
+    return res
 
 
 def find_tightest_lambda(n, delta, req_alpha, risk_evaluator):
-    lambdas = np.linspace(2, 10, 100)
-    min_lambda = 10
-    for lmbd in lambdas:
-        # print(f'lambda={lmbd}')
-        alpha = find_lambda_ucb(n, lmbd, risk_evaluator, delta)
-        if alpha < req_alpha:
-            min_lambda = lmbd
-            break
-    return min_lambda
+    lmbd = brentq(lambda lmbd: find_lambda_ucb(n, lmbd, risk_evaluator, delta) - req_alpha, 1e-10, 1.2)
+    return lmbd

unit_test.py
@@ -17,8 +17,8 @@ def lambda_wrapper(lmbd):
 
 
 def rank_loss(label, prediction_set):
-    r_loss = (((label[:, 0] > 0) * (np.min(prediction_set, axis=1) < 0))).sum() + (
-                ((label[:, 0] < 0) * (np.max(prediction_set, axis=1) > 0))).sum()
+    r_loss = ((((label > 0) * (np.max(prediction_set, axis=1) < 0))).sum() + (
+                ((label < 0) * (np.min(prediction_set, axis=1) > 0))).sum()) / len(label)
     # print(f'r_loss={r_loss}')
     return r_loss
 
@@ -28,14 +28,14 @@ def evaluator(lmbd):
     # print(f'data_valid.shape={data_valid.shape}')
     predict = lambda_wrapper(lmbd)
     prediction_set = predict()
-    return rank_loss(model.X_val, prediction_set)
+    return rank_loss(model.y_val, prediction_set)
 
 if __name__ == '__main__':
-    # model = LogisticRanking()
+    model = LogisticRanking()
     # model = SvmRanking()
-    model = DecisionTreeRanking()
+    # model = DecisionTreeRanking()
     model.train_model()
-    min_lambda = find_tightest_lambda(100, 0.01, 5, evaluator)
+    min_lambda = find_tightest_lambda(26048, 0.1, 0.05, evaluator)
     print('#############################################################################')
     print(f'min_lambda={min_lambda}')
     print('#############################################################################')